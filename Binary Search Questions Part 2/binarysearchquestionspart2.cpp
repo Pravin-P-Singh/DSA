d1cdfcbae1a4cfe172bf9c8a44a5442a3a3537f8
Pravin Singh <pravin130204@gmail.com>
2023-08-20 17:50:31 +0530

    Added Binary Search Questions Part 2

--- Binary Search Questions Part 2/binarysearchquestionspart2.cpp
+++ Binary Search Questions Part 2/binarysearchquestionspart2.cpp
@@ -77,3 +77,87 @@ int pivot(int arr[], int n) {
 //         return binarySearch(arr, 0, pivot-1, k);
 //     }
 // }
+
+//*******************************************************
+// Find square root using binary search
+
+// long long int binarySearch(int n){
+//     int s = 0;
+//     int e = n;
+//     long long int mid = s + (e-s)/2;
+//     long long int ans = -1;
+//     while(s<=e){
+//         long long int square = mid*mid;
+//         if(square == n){
+//             return mid;
+//         }
+//         else if(square < n){
+//             ans = mid;
+//             s = mid + 1;
+//         }
+//         else{
+//             e = mid-1;
+//         }
+//         mid = s + (e-s)/2;
+//     }
+//     return ans;
+// }
+
+// int floorSqrt(int n)
+// {
+//     // Write your code here.
+//     return binarySearch(n);
+// }
+
+// *********************************************
+// Same above question with precision
+
+// #include <iostream>
+
+// using namespace std;
+// long long int binarySearch(int n){
+//     int s = 0;
+//     int e = n;
+//     long long int mid = s + (e-s)/2;
+//     long long int ans = -1;
+//     while(s<=e){
+//         long long int square = mid*mid;
+//         if(square == n){
+//             return mid;
+//         }
+//         else if(square < n){
+//             ans = mid;
+//             s = mid + 1;
+//         }
+//         else{
+//             e = mid-1;
+//         }
+//         mid = s + (e-s)/2;
+//     }
+//     return ans;
+// }
+
+// double morePrecision(int tempSol, int precision, int n){
+//     double factor = 1, ans = -1;
+//     for(int i = 0; i < precision; i++){
+//         factor = factor/10;
+//         for(double j = tempSol; j*j <= n; j += factor){
+//             ans = j;
+//         }
+//     }
+//     return ans;
+// }
+
+// double floorSqrt(int n)
+// {
+//     // Write your code here.
+//     double tempSol = binarySearch(n);
+//     return morePrecision(tempSol,3,n);
+// }
+
+// int main()
+// {
+//     cout<< floorSqrt(37);
+
+//     return 0;
+// }
