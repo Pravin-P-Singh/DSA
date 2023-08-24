e2ef73c5c2265bba19e4e85bf52a4ef8ef736558
Pravin Singh <pravin130204@gmail.com>
2023-08-24 18:58:13 +0530

    Added Aggressive Cows Problem

--- Binary Search Questions Part 3/binarySearchQuestionPart3.cpp
+++ Binary Search Questions Part 3/binarySearchQuestionPart3.cpp
@@ -83,3 +83,44 @@
 //     }
 //     return mid;
 // }
+
+//**********************************************************
+//Aggressive cows problem
+bool isPossible(vector<int> &stalls, int k, int mid){
+    int CowCount = 1;
+    int lastPos = stalls[0];
+    for(int i = 0;i < stalls.size(); i++ ){
+        if(stalls[i]-lastPos>=mid){
+            CowCount++;
+            if(CowCount==k){
+                return true;
+            }
+            lastPos = stalls[i];
+        }
+    }
+    return false;
+}
+
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    //    Write your code here.
+    sort(stalls.begin(),stalls.end());
+    int s = 0;
+    int maxi = -1,ans = -1;
+    for(int i = 0; i < stalls.size();i++){
+        maxi = max(maxi,stalls[i]);
+    }
+    int e = maxi;
+    int mid = s + (e-s)/2;
+    while(s<=e){
+        if(isPossible(stalls,k,mid)){
+            s = mid + 1;
+            ans = mid;
+        }
+        else{
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
