1aeddc68ab4fa115bb7d8a87f9e6d71782fcf3aa
Pravin Singh <pravin130204@gmail.com>
2023-09-19 18:43:47 +0530

    Optimised Bubble Sort

--- Bubble Sort/bubblesort.cpp
+++ Bubble Sort/bubblesort.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
-using namespace std;
 #include <bits/stdc++.h> 
+using namespace std;
 void bubbleSort(vector<int>& arr, int n)
 {   
     // Write your code here.
+    bool swapped = false;
     for(int i = 1 ; i < n ; i++){
+        // Bahar ka loop chalaya
         for(int j = 0 ; j < n - i ; j++){
+            // Andar loop chalaya aur isme j ko j+1 se compare karake swap karaya taki har round mai corresponding largest element uske sahi jagah pe place ho jaye
             if(arr[j] > arr[j+1] ){
                 swap(arr[j+1],arr[j]);
+                swapped = true;
             }
+        }
+        if(swapped == false){
+            break;
         }
     }
 }
+
 
 int main() {
   cout << "Hello World!\n";
