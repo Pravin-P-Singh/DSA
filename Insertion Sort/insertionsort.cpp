23de3d9c78ee1dd67c9c6601b1a1227baa8665e4
Pravin Singh <pravin130204@gmail.com>
2023-09-20 20:40:21 +0530

    Added Insertion Sort

---
+++ Insertion Sort/insertionsort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+void insertionSort(int n, vector<int> &arr){
+    // Write your code here.
+    for(int i = 1; i < n;i++){
+        int temp = arr[i];
+        int j = i - 1;
+        for(;j>=0;j--){
+            if(temp < arr[j]){
+                arr[j+1] = arr[j];
+            }
+            else{
+                break;
+            }
+        }
+        arr[j+1]=temp;
+    }
+}
+int main() {
+  std::cout << "Hello World!\n";
+}
